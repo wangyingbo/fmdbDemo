187a4623952b21a4f9b3657948802c472d2c37b6
wangyingbo <wangyingbo528@126.com>
2019-09-03 12:31:15 +0800

    .

--- fmdbDemo/ViewController.m
+++ fmdbDemo/ViewController.m
@@ -14,10 +14,44 @@
 
 @implementation ViewController
 
+#pragma mark - override
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
+#pragma mark - configData
 
+
+#pragma mark - other
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    NSString *str = @"http://www.jianshu.com/p/6930f335adba";
+    NSURL *url = [NSURL URLWithString:str];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSURLSession *session = [NSURLSession sharedSession];
+    
+    dispatch_semaphore_t sem = dispatch_semaphore_create(0);
+    __block NSUInteger count = 0;
+    for (int i=0; i<10; i++) {
+        
+        NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            
+            NSLog(@"%d---%d",i,i);
+            count++;
+            if (count==10) {
+                dispatch_semaphore_signal(sem);
+                count = 0;
+            }
+            
+        }];
+        
+        [task resume];
+    }
+    dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER);
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSLog(@"end");
+    });
+}
 @end
